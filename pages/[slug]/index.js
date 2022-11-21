286bb98e8b70b9a262a24e650575a364521c91cc
Kelly Packer <kelly@kellypacker.com>
2022-11-21 14:50:55 -0700

    Add props for test slug

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
-export default function TestNested() {
+export const getStaticPaths = async () => {
+    return {
+        paths: [{ params: { slug: 'test-slug' } }],
+        fallback: true,
+    };
+};
+
+export const getStaticProps = async ({ params = {}, preview = false }) => {
+    const slug = String(params.slug);
+
+    return {
+        props: {
+            slug,
+        },
+    };
+};
+
+export default function TestNested(props) {
+    const {slug} = props;
     return (
         <>
-            <h1>TEST Slug</h1>
+            <h1>TEST {slug}</h1>
 
             <h1 className="title">
                 <Link href="/">Back to Home</Link>
