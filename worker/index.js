70e0f0fecb35a5d23938ef12b6acc7a274eaabfb
Kelly Packer <kelly@kellypacker.com>
2022-11-22 12:41:31 -0700

    Fix for delete

--- worker/index.js
+++ worker/index.js
@@ -2,18 +2,14 @@
 
 const fetchCachedNextData = (event) => {
     const url = new URL(event.request.url);
+    console.log({event});
     
     // e.g.`workbox-precache-v2-http://localhost:4200/`;
     const cacheName = `workbox-precache-v2-${location.origin}/`;
-    
-   
 
     // match request to cache, also fetch request. 
     // update cache and serve what is available
     event.respondWith(caches.open(cacheName).then((cache) => {
-
-        
-
         return cache.matchAll(event.request, {ignoreSearch: true})
             .then((responses) => {
 
@@ -24,37 +20,50 @@ const fetchCachedNextData = (event) => {
                     return networkResponse;
                 });
                 // remove older versions of the page
-            //    cache.keys().then((keys) => {
-            //        // get file name
+               cache.keys().then((keys) => {
+                   // get file name
                    
-            //        const getFileName = (url) => {
-            //            const findFile = new RegExp(/\/(.*?).json/gm);
-            //            const matches = findFile.exec(url);
-            //            const fileParts = matches[1].split('/');
-            //            return fileParts[fileParts.length - 1];
-            //        };
-            //        const fileName = getFileName(url.href)
-            //        console.log(fileName);
-            //        console.log({keys});
+                   const getFileName = (url) => {
+                       const findFile = new RegExp(/\/(.*?).json/gm);
+                       const matches = findFile.exec(url);
+                       const fileParts = matches[1].split('/');
+                       return fileParts[fileParts.length - 1];
+                   };
+                   const fileName = getFileName(url.href)
+                   console.log(fileName);
+                   console.log({keys});
 
-            //        // find all cached versions of this page
-            //        const matches = keys.filter(request => {
-            //            return request.url.includes(fileName);
-            //        });
-            //        console.log({matches});
+                   // find all cached versions of this page
+                   const matches = keys.filter(request => {
+                       return request.url.includes(fileName);
+                   });
+                   console.log({matches}, url.href);
 
-            //        if (matches.length > 1) {
-            //            // filter out current request
-            //            // also files with __WB_REVISION__ - not exactly sure what part they play
-            //            const toDelete = matches.filter(request => {
-            //                return (cachedResponse && request.url !== cachedResponse.url) || (fetchedResponse && request.url !== fetchedResponse.url);
-            //            });
-            //            console.log(toDelete);
-            //            toDelete.forEach((request, index, array) => {
-            //                cache.delete(request);
-            //            });
-            //        }
-            //    });
+                   if (matches.length > 0) {
+                        try {
+                            // filter out current request
+                            // also files with __WB_REVISION__ - not exactly sure what part they play
+                            const toDelete = matches.filter(request => {
+                                console.log(request.url, url.href);
+                                if (request.url.includes('__WB_REVISION__')) {
+                                    return false;
+                                }
+                                if (request.url !== url.href) {
+                                    return false;
+                                }
+                                return true;
+                            });
+                            console.log({toDelete});
+                            if (toDelete.length > 0) {
+                                toDelete.forEach((request, index, array) => {
+                                    cache.delete(request);
+                                });
+                            }
+                        } catch(e) {
+                            console.log(e);
+                        }
+                   }
+               });
 
                 if (cachedResponse) {
                     console.log('FOUND', url.href);
@@ -74,7 +83,7 @@ const fetchCachedNextData = (event) => {
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
   
-    const isNextData = url.href.includes('/_next/data');
+    const isNextData = url.href.includes('/_next/data') && event.request.method !== 'HEAD';
   
     if (isNextData) {
       return fetchCachedNextData(event);
