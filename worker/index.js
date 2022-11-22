77d1e9e596cfda0e11ee7bb0b8e6907b76af555a
Kelly Packer <kelly@kellypacker.com>
2022-11-22 10:07:45 -0700

    Move cache

--- worker/index.js
+++ worker/index.js
@@ -6,42 +6,45 @@ const fetchCachedNextData = (event) => {
     // e.g.`workbox-precache-v2-http://localhost:4200/`;
     const cacheName = `workbox-precache-v2-${location.origin}/`;
     
-    // remove older versions of the page
-    cache.keys().then((keys) => {
-        // get file name
-        
-        const getFileName = (url) => {
-            const findFile = new RegExp(/\/(.*?).json/gm);
-            const matches = findFile.exec(url);
-            const fileParts = matches[1].split('/');
-            return fileParts[fileParts.length - 1];
-        };
-        const fileName = getFileName(url.href)
-        console.log(fileName);
-        console.log({keys});
-
-        // find all cached versions of this page
-        const matches = keys.filter(request => {
-            return request.url.includes(fileName);
-        });
-        console.log({matches});
-
-        if (matches.length > 1) {
-            // filter out current request
-            // also files with __WB_REVISION__ - not exactly sure what part they play
-            const toDelete = matches.filter(request => {
-                return request.url !== cachedResponse.url;
-            });
-            console.log(toDelete);
-            toDelete.forEach((request, index, array) => {
-                cache.delete(request);
-            });
-        }
-    });
+   
 
     // match request to cache, also fetch request. 
     // update cache and serve what is available
     event.respondWith(caches.open(cacheName).then((cache) => {
+        
+         // remove older versions of the page
+        cache.keys().then((keys) => {
+            // get file name
+            
+            const getFileName = (url) => {
+                const findFile = new RegExp(/\/(.*?).json/gm);
+                const matches = findFile.exec(url);
+                const fileParts = matches[1].split('/');
+                return fileParts[fileParts.length - 1];
+            };
+            const fileName = getFileName(url.href)
+            console.log(fileName);
+            console.log({keys});
+
+            // find all cached versions of this page
+            const matches = keys.filter(request => {
+                return request.url.includes(fileName);
+            });
+            console.log({matches});
+
+            if (matches.length > 1) {
+                // filter out current request
+                // also files with __WB_REVISION__ - not exactly sure what part they play
+                const toDelete = matches.filter(request => {
+                    return request.url !== cachedResponse.url;
+                });
+                console.log(toDelete);
+                toDelete.forEach((request, index, array) => {
+                    cache.delete(request);
+                });
+            }
+        });
+
         return cache.matchAll(event.request, {ignoreSearch: true})
             .then((responses) => {
 
