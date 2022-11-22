a00b6b86a9c4b9fb85530b4f71fa331c3c36e4cf
Kelly Packer <kelly@kellypacker.com>
2022-11-22 10:43:44 -0700

    Cache test

--- worker/index.js
+++ worker/index.js
@@ -24,37 +24,37 @@ const fetchCachedNextData = (event) => {
                     return networkResponse;
                 });
                 // remove older versions of the page
-               cache.keys().then((keys) => {
-                   // get file name
+            //    cache.keys().then((keys) => {
+            //        // get file name
                    
-                   const getFileName = (url) => {
-                       const findFile = new RegExp(/\/(.*?).json/gm);
-                       const matches = findFile.exec(url);
-                       const fileParts = matches[1].split('/');
-                       return fileParts[fileParts.length - 1];
-                   };
-                   const fileName = getFileName(url.href)
-                   console.log(fileName);
-                   console.log({keys});
+            //        const getFileName = (url) => {
+            //            const findFile = new RegExp(/\/(.*?).json/gm);
+            //            const matches = findFile.exec(url);
+            //            const fileParts = matches[1].split('/');
+            //            return fileParts[fileParts.length - 1];
+            //        };
+            //        const fileName = getFileName(url.href)
+            //        console.log(fileName);
+            //        console.log({keys});
 
-                   // find all cached versions of this page
-                   const matches = keys.filter(request => {
-                       return request.url.includes(fileName);
-                   });
-                   console.log({matches});
+            //        // find all cached versions of this page
+            //        const matches = keys.filter(request => {
+            //            return request.url.includes(fileName);
+            //        });
+            //        console.log({matches});
 
-                   if (matches.length > 1) {
-                       // filter out current request
-                       // also files with __WB_REVISION__ - not exactly sure what part they play
-                       const toDelete = matches.filter(request => {
-                           return (cachedResponse && request.url !== cachedResponse.url) || (fetchedResponse && request.url !== fetchedResponse.url);
-                       });
-                       console.log(toDelete);
-                       toDelete.forEach((request, index, array) => {
-                           cache.delete(request);
-                       });
-                   }
-               });
+            //        if (matches.length > 1) {
+            //            // filter out current request
+            //            // also files with __WB_REVISION__ - not exactly sure what part they play
+            //            const toDelete = matches.filter(request => {
+            //                return (cachedResponse && request.url !== cachedResponse.url) || (fetchedResponse && request.url !== fetchedResponse.url);
+            //            });
+            //            console.log(toDelete);
+            //            toDelete.forEach((request, index, array) => {
+            //                cache.delete(request);
+            //            });
+            //        }
+            //    });
 
                 if (cachedResponse) {
                     console.log('FOUND', url.href);
